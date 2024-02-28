574ac69c9703c8526d528e734d550b6151929bd4
Jorge Lozano <jorge@MacBook-Pro-de-Jorge.local>
2024-02-28 10:16:36 -0500

    add and test output invalidation id

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ distribution_id = os.getenv('cloudfront-distribution-id')
 invalidation_path = os.getenv('cloudfront-invalidation-path')
   
 def create_invalidation_cache(distribution_id, invalidation_path):
-    print(f"Distribution id", distribution_id)
     res = cf.create_invalidation(
         DistributionId=distribution_id,
         InvalidationBatch={
@@ -26,4 +25,5 @@ def create_invalidation_cache(distribution_id, invalidation_path):
  
 # Create CloudFront Invalidation
 id = create_invalidation_cache(distribution_id, invalidation_path)
+os.environ['INVALIDATION_ID'] = id
 print("Invalidation created successfully with Id: " + id)
